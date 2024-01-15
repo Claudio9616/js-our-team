4475a08cb956fcee65319b00725c6c6650a87d65
Claudio9616 <clares9611@gmail.com>
2024-01-15 16:31:06 +0100

    add js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('ok')
+// Wayne Barnett |	Founder & CEO        |	wayne-barnett-founder-ceo.jpg
+// Angela Caroll |	Chief Editor         |	angela-caroll-chief-editor.jpg
+// Walter Gordon |	Office Manager       |	walter-gordon-office-manager.jpg
+// Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
+// Scott Estrada |	Developer            |	scott-estrada-developer.jpg
+// Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg
+const team = {
+    name: ['Wayne Barnett', 'Angela Caroll'],
+    role: ['Founder & CEO', 'Chief Editor'],
+    pic: [`<img src="img/wayne-barnett-founder-ceo.jpg">`, `<img src="img/angela-caroll-chief-editor.jpg" alt="">`]
+}
